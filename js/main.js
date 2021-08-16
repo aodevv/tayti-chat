30ad12240acd51079ed063e11f5fc8a630e28a29
l3edssa <aimarah.oussam@gmail.com>
2021-08-16 18:39:13 +0100

    finished left controls functionality

--- js/main.js
+++ js/main.js
@@ -1,60 +1,72 @@
 //Contacts CONTROLLER
-var contactsController = (function(){
-    var Contact = function(id, name){
-        this.id = id;
-        this.name = name;
-    }
+var contactsController = (function () {
+  var Contact = function (id, name) {
+    this.id = id;
+    this.name = name;
+  };
 })();
 
 // ChAT CONTROLLER
 var chatController = (function () {
-    var Message = function(value, time){
-        this.value = value;
-        this.time = time;
-    }
+  var Message = function (value, time) {
+    this.value = value;
+    this.time = time;
+  };
 
-
-    return {
-        addChat: function(value){
-            var newChat, curTime;
-            var d = new Date();
-            curTime = `${d.getHours()}:${d.getMinutes()}`;
-            newChat = new Message(value, curTime);
-            return newChat;
-        }
-        
-    }
+  return {
+    addChat: function (value) {
+      var newChat, curTime;
+      var d = new Date();
+      curTime = `${d.getHours()}:${d.getMinutes()}`;
+      newChat = new Message(value, curTime);
+      return newChat;
+    },
+  };
 })();
 
 // UI CONTROLLER
 var UIController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    msgBtn: ".msg-btn",
+    msgText: ".msg-input",
+    chatList: ".message-list",
+    chatContainer: ".right__center",
+    blThumbnail: ".settings-thumbnail", //Bottom left user thumbnail
+    logoutPopup: ".logout-popup", //Bottom left user thumbnail
+    leftBtnGroup: ".left__wrapper--center",
+    leftBtnList: ".icons__wrapper",
+    centerContainer: ".center__wrapper",
+  };
 
-    var DOMstrings = {
-        inputType: '.add__type',
-        msgBtn: '.msg-btn',
-        msgText: '.msg-input',
-        chatList: '.message-list',
-        chatContainer: '.right__center',
-        blThumbnail: '.settings-thumbnail', //Bottom left user thumbnail
-        logoutPopup: '.logout-popup' //Bottom left user thumbnail
-    }
-    
-    return{
-        getInput: function(){
-            return document.querySelector(DOMstrings.msgText).value;
-        },
+  var clearCenter = () => {
+    var centerChildren = document.querySelector(
+      DOMstrings.centerContainer
+    ).children;
+    console.log(centerChildren);
+    [...centerChildren].forEach((el) => {
+      el.classList.add("d-none");
+    });
+  };
 
-        addChatMessage: function(newMsg){
-            console.log(newMsg);
-            var isRight, lastMessage;
-            //1. get the last li element in the ul messages list
-            lastMessage = document.querySelector(DOMstrings.chatList).lastElementChild;
+  return {
+    getInput: function () {
+      return document.querySelector(DOMstrings.msgText).value;
+    },
 
-            //2. Check if last message is already a sent message ie. in the right 
-            isRight = lastMessage.classList.contains('right-chat');
+    addChatMessage: function (newMsg) {
+      console.log(newMsg);
+      var isRight, lastMessage;
+      //1. get the last li element in the ul messages list
+      lastMessage = document.querySelector(
+        DOMstrings.chatList
+      ).lastElementChild;
 
-            //3. Prepare markup for insertion
-            markup = `
+      //2. Check if last message is already a sent message ie. in the right
+      isRight = lastMessage.classList.contains("right-chat");
+
+      //3. Prepare markup for insertion
+      markup = `
             <li class="right-chat" style="opacity: 0; transform: translateY(20px)">
                 <div class="conversation-item u-mb-s">
                     <div class="user-thumbnail"></div>
@@ -69,119 +81,148 @@ var UIController = (function () {
             </li>
             `;
 
-            //4. Remove user thumbnail and name if last message is on the right
-            if(isRight){
-                lastMessage.firstElementChild.firstElementChild.classList.add('visi-hidden');
-                lastMessage.firstElementChild.lastElementChild.lastElementChild.remove();   
-            }
+      //4. Remove user thumbnail and name if last message is on the right
+      if (isRight) {
+        lastMessage.firstElementChild.firstElementChild.classList.add(
+          "visi-hidden"
+        );
+        lastMessage.firstElementChild.lastElementChild.lastElementChild.remove();
+      }
 
-            //5. Insert new message
-            document.querySelector(DOMstrings.chatList).insertAdjacentHTML('beforeend', markup);
+      //5. Insert new message
+      document
+        .querySelector(DOMstrings.chatList)
+        .insertAdjacentHTML("beforeend", markup);
 
-            //6. Animating new message insertion fade in from bottom
-            lastMessage = document.querySelector(DOMstrings.chatList).lastElementChild;
-            setTimeout(function(){
-                lastMessage.style.opacity = "1";
-                lastMessage.style.transform = "translateY(0)";
-            }, 10)
-            
+      //6. Animating new message insertion fade in from bottom
+      lastMessage = document.querySelector(
+        DOMstrings.chatList
+      ).lastElementChild;
+      setTimeout(function () {
+        lastMessage.style.opacity = "1";
+        lastMessage.style.transform = "translateY(0)";
+      }, 10);
+    },
 
-        },
+    clearMessageInput: function () {
+      var field = document.querySelector(DOMstrings.msgText);
+      field.value = "";
+      field.focus();
+    },
 
-        clearMessageInput: function(){
-            var field = document.querySelector(DOMstrings.msgText);
-            field.value = '';
-            field.focus();
-        },
+    scrollChatContainer: function () {
+      var chatScroll = document.querySelector(DOMstrings.chatContainer);
+      chatScroll.scrollTop = chatScroll.scrollHeight;
+    },
 
-        scrollChatContainer: function(){
-            var chatScroll = document.querySelector(DOMstrings.chatContainer);
-            chatScroll.scrollTop = chatScroll.scrollHeight;
-        },
+    toggleLogoutPopup: function () {
+      var popup = document.querySelector(DOMstrings.logoutPopup);
+      popup.classList.toggle("user-thumbnail__popup--shown");
+      /* if (popup.classList.contains("d-none")) {
+        popup.classList.remove("d-none");
+        //popup.style.opacity = "1";
+        setTimeout(function () {
+          popup.style.opacity = "1";
+          popup.style.transform = "translate(5%, -70%)";
+        }, 10);
+      } else {
+        popup.style.opacity = "0";
+        setTimeout(function () {
+          popup.classList.add("d-none");
+          popup.style.transform = "translate(5%, -60%)";
+        }, 300);
+      } */
+    },
 
-        toggleLogoutPopup: function(){
-            var popup = document.querySelector(DOMstrings.logoutPopup);
-            //popup.classList.toggle("d-none");
-            if(popup.classList.contains("d-none")){
-                popup.classList.remove("d-none");
-                //popup.style.opacity = "1";
-                setTimeout(function(){
-                popup.style.opacity = "1";
-                popup.style.transform = "translate(5%, -70%)"
-            }, 10);
-            }else{
-                popup.style.opacity = "0";
-                 setTimeout(function(){
-                popup.classList.add("d-none");
-                popup.style.transform = "translate(5%, -60%)"
-            }, 500);
-            }
+    toggleCenterTabs: function (target) {
+      if (target.tagName == "I") {
+        if (
+          !target.parentNode.parentNode.classList.contains(
+            "icons__wrapper--active"
+          )
+        ) {
+          var btnList = document.querySelector(DOMstrings.leftBtnList).children;
+          var id = target.parentNode.parentNode.id;
 
-        },
-
-
-        getDOMstrings: function(){
-            return DOMstrings;
+          for (var i = 0; i < btnList.length; i++) {
+            if (btnList[i].classList.contains("icons__wrapper--active"))
+              btnList[i].classList.remove("icons__wrapper--active");
+          }
+          target.parentNode.parentNode.classList.add("icons__wrapper--active");
+          clearCenter();
+          document.querySelector(`.${id}`).classList.remove("d-none");
         }
-        
-    }
+      }
+    },
 
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (Cc, UIC) {
+  var setupEventListeners = function () {
+    UIC.scrollChatContainer();
+    var DOM = UIC.getDOMstrings();
+    document
+      .getElementsByTagName("form")[0]
+      .addEventListener("click", function (event) {
+        event.preventDefault();
+        //ctrlAddChatMsg();
+      });
 
-    var setupEventListeners = function(){
-        UIC.scrollChatContainer();
-        var DOM = UIC.getDOMstrings();
-        document.getElementsByTagName("form")[0].addEventListener("click", function(event){
-            event.preventDefault();
-            //ctrlAddChatMsg();
-          });
-
-        document.querySelector(DOM.msgBtn).addEventListener("click",ctrlAddChatMsg);
-        document.querySelector(DOM.msgText).addEventListener("keypress", function (e) {
-            if(e.key === 'Enter'){
-                ctrlAddItem();
-            }
-        });
-        document.querySelector(DOM.blThumbnail).addEventListener("click", ctrlTogglePopup);
-    }
-
-    var ctrlAddChatMsg = () => {
-        var input, newMsg;
-
-        //1. Get message field input data
-        input = UIC.getInput();
-        if(input){
-            
-            //2. Add chat message to chat controller
-            newMsg = Cc.addChat(input);
-            
-
-            //3. Add chat message to UI
-            UIC.addChatMessage(newMsg);
-            UIC.scrollChatContainer();
-            UIC.clearMessageInput();
-
+    document
+      .querySelector(DOM.msgBtn)
+      .addEventListener("click", ctrlAddChatMsg);
+    document
+      .querySelector(DOM.msgText)
+      .addEventListener("keypress", function (e) {
+        if (e.key === "Enter") {
+          ctrlAddItem();
         }
+      });
+    document
+      .querySelector(DOM.blThumbnail)
+      .addEventListener("click", ctrlTogglePopup);
+    document
+      .querySelector(DOM.leftBtnGroup)
+      .addEventListener("click", function (e) {
+        ctrlCenterLayout(e.target);
+      });
+  };
+
+  var ctrlAddChatMsg = () => {
+    var input, newMsg;
+
+    //1. Get message field input data
+    input = UIC.getInput();
+    if (input) {
+      //2. Add chat message to chat controller
+      newMsg = Cc.addChat(input);
+
+      //3. Add chat message to UI
+      UIC.addChatMessage(newMsg);
+      UIC.scrollChatContainer();
+      UIC.clearMessageInput();
     }
+  };
 
-    var ctrlTogglePopup = () => {
-        UIC.toggleLogoutPopup();
+  var ctrlTogglePopup = () => {
+    UIC.toggleLogoutPopup();
+  };
 
-    }
+  var ctrlCenterLayout = (target) => {
+    UIC.toggleCenterTabs(target);
+  };
 
-
-    return{
-        init: function(){
-            console.log("App started");
-            setupEventListeners();
-        }
-    }
-
- 
+  return {
+    init: function () {
+      console.log("App started");
+      setupEventListeners();
+    },
+  };
 })(chatController, UIController);
-
 
 controller.init();
